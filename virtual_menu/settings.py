e56a1ae74fe02cdbc9e33d6c8487dd8fb2e27850
willy <benjamimwilly@protonmail.com>
2021-12-27 11:41:56 -0300

    beta on heroku

--- virtual_menu/settings.py
+++ virtual_menu/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,3 +158,4 @@ AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = '/cardapio/pedidos/'
 LOGOUT_REDIRECT_URL = '/'
 
+django_on_heroku.settings(locals())
