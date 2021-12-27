4607371f2d830fdb2e0a48acd125fe80014fde70
willy <benjamimwilly@protonmail.com>
2021-12-27 15:53:02 -0300

    see it working

--- virtual_menu/settings.py
+++ virtual_menu/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-##web: gunicorn virtual_menu.wsgi
+##
 import os
 from pathlib import Path
 import django_on_heroku
