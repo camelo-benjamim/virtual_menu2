9216866e4899ce1dc85bab6c7b5be60071659915
willy <benjamimwilly@protonmail.com>
2022-01-11 11:21:38 -0300

    staticfiles adicionado

--- virtual_menu/settings/base.py
+++ virtual_menu/settings/base.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 #Location of static files
-##STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT  = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
